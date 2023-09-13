247c45f915d8bec9a8cb50e054bd5cd9dce1009a
kaynekaynekayne <theygoon@gmail.com>
2023-09-14 01:10:23 +0900

    c

---
+++ 3. 숫자야구/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports={
+    entry:{
+        app:'./main.js', //하나로 합쳐질 파일의 이름
+    },
+    module:{
+        rules:[{
+
+        }],
+    },
+    plugins:[],
+    output:{
+        filename:'[name].js',
+        path:'./dist',
+    },
+}
