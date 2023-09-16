9373b0fee8d53152b7ee792a0f58f1c0cb70d43d
kaynekaynekayne <theygoon@gmail.com>
2023-09-16 21:39:58 +0900

    m

--- 3. 숫자야구/main.js
+++ 3. 숫자야구/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
 
+import NumberBaseball from './Numberbaseball.vue'
 
-new Vue().$mount('#root');
+new Vue(NumberBaseball).$mount('#root');
--- 3. 숫자야구/webpack.config.js
+++ 3. 숫자야구/webpack.config.js
@@ -1,3 +1,4 @@
+const {vueLoaderPlugin}=require('vue-loader/lib/plugin');
 const path=require('path');
 
 module.exports={
@@ -6,10 +7,13 @@ module.exports={
     },
     module:{
         rules:[{
-
+            test: /\.vue$/,
+            loader: 'vue-loader'
         }],
     },
-    plugins:[],
+    plugins:[
+        new vueLoaderPlugin()
+    ],
     output:{
         filename:'[name].js',
         path:path.join(__dirname, 'dist'),
