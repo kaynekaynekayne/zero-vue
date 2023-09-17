4375f70588e635f7c95085f8c8cdcbb16c012b55
kaynekaynekayne <theygoon@gmail.com>
2023-09-17 22:54:37 +0900

    'm'

--- 4/test/src/main.js
+++ 4/test/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-// import App from './App.vue'
-import LottoGenerator from './LottoGenerator.vue'
+import App from './App.vue'
+// import LottoGenerator from './LottoGenerator.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(LottoGenerator),
+  render: h => h(App),
 }).$mount('#app')
